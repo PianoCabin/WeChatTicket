5528182a7b2ce6c21ff0c27a2e335fb97fba6c75
Venessa-wei <fzw_16@163.com>
2018-10-15 17:23:38 +0800

    finish the fifth Api, admin logout

--- adminpage/views.py
+++ adminpage/views.py
@@ -5,16 +5,28 @@ from django.contrib import auth
 
 
 class Login(APIView):
-
     def get(self):
         if not self.request.user.is_authenticated():
-            raise ValidateError("Please Login!")
+            raise ValidateError("Please login!")
 
     def post(self):
         self.check_input("username", "password")
         username = self.input["username"]
         password = self.input["password"]
         user = auth.authenticate(request=self.request, username=username, password=password)
-        if user:
-            raise ValidateError("Error input, please input again!")
+        if not user:
+            raise ValidateError("Either username or password is wrong, please login again!")
+
         auth.login(self.request, user)
+
+
+class Logout(APIView):
+    def post(self):
+        if not self.request.user.is_authenticated():
+            raise ValidateError("Logout failed, you must login first!")
+        auth.logout(self.request)
+
+
+class ActivityList(APIView):
+    def post(self):
+        pass
