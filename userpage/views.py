c7fb3a1d03db82975f285739064b33a7f1472028
linlinhaohao888 <linlinhaohao.888@163.com>
2018-10-17 02:21:45 +0800

    First Test(API 1)

--- userpage/views.py
+++ userpage/views.py
@@ -14,8 +14,9 @@ class UserBind(APIView):
         raise NotImplementedError('You should implement UserBind.validate_user method')
 
     def get(self):
+
         self.check_input('openid')
-        return User.get_by_openid(self.input['openid']).student_id
+        return str(User.get_by_openid(self.input['openid']).student_id or '')
 
     def post(self):
         self.check_input('openid', 'student_id', 'password')
